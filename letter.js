4245116560a3021914cfac472e43e33e2bfab61e
Jaime Tyler <jaimetyler88@gmail.com>
2019-05-02 08:38:20 -0400

    started creating Word constructor - accessing the Letter constructor from letter.js / made an empty array that will take in the letters guessed and split them with spaces / made a function that will return the word as a string, called on each letter object to either display the correct letter guessed or an underscore to indicate letter not guessed.

--- letter.js
+++ letter.js
@@ -1,3 +1,13 @@
+// Letter.js: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
+
+// A string value to store the underlying character for the letter
+
+// A boolean value that stores whether that letter has been guessed yet
+
+// A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+
+// A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+
 var Letter = function(letter) {
     this.letter = letter;
     this.guessed = false;
@@ -10,7 +20,9 @@ var Letter = function(letter) {
     }
     this.check = function(value) {
         if (value === this.letter) {
-            return true;
+            return true
         }
     }
 }
+
+module.exports = Letter;
